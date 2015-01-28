4db1e9388df2f942e1181931f7b54b1440975f20
Prelang Builder <builder@prelang.com>
2015-01-28 16:17:06 +0000

    Defined validation for attribute 'ip_address' on model 'Registration'

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,4 +1,5 @@
 class Registration < ActiveRecord::Base
   validates_formatting_of :email, using: :email
   validates_formatting_of :resumelink, using: :url
+  validates_formatting_of :ip_address, using: :ip_address_v4
 end
